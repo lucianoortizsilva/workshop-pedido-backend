d1640afec615c1d3f73e71f020bb58f2505ddca1
Luciano Ortiz Silva <lucianoortizsilva@gmail.com>
2019-11-22 10:42:25 -0300

    Projeto criado

---
+++ src/main/java/com/lucianoortizsilva/workshoppedido/WorkshopPedidoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lucianoortizsilva.workshoppedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopPedidoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopPedidoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucianoortizsilva/workshoppedido/WorkshopPedidoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianoortizsilva.workshoppedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopPedidoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
