5c1a6d7bd42a673f3ba7caab9218a09ef394a555
Luciano Ortiz Silva <lucianoortizsilva@gmail.com>
2019-11-22 11:20:11 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/lucianoortizsilva/workshoppedido/resources/CategoriaResource.java
+++ src/main/java/com/lucianoortizsilva/workshoppedido/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package com.lucianoortizsilva.workshoppedido.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucianoortizsilva.workshoppedido.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String list() {
-		return "REST ok";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 
 }
