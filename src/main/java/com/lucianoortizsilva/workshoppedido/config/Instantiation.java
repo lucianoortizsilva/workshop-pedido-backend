7f94452fac0bd529f50a1d3753461eae69e9b04c
Luciano Ortiz Silva <lucianoortizsilva@gmail.com>
2019-11-25 13:56:31 -0300

    adicionando mapeamento de cidades e estado (UF)

--- src/main/java/com/lucianoortizsilva/workshoppedido/config/Instantiation.java
+++ src/main/java/com/lucianoortizsilva/workshoppedido/config/Instantiation.java
@@ -7,8 +7,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.lucianoortizsilva.workshoppedido.domain.Categoria;
+import com.lucianoortizsilva.workshoppedido.domain.Cidade;
+import com.lucianoortizsilva.workshoppedido.domain.Estado;
 import com.lucianoortizsilva.workshoppedido.domain.Produto;
 import com.lucianoortizsilva.workshoppedido.repositories.CategoriaRepository;
+import com.lucianoortizsilva.workshoppedido.repositories.CidadeRepository;
+import com.lucianoortizsilva.workshoppedido.repositories.EstadoRepository;
 import com.lucianoortizsilva.workshoppedido.repositories.ProdutoRepository;
 
 @Configuration
@@ -16,33 +20,50 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
-	@Autowired
-	private ProdutoRepository produtoRepository; 
 
-		
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
 		this.categoriaRepository.deleteAll();
-		
+		this.produtoRepository.deleteAll();
+
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
+
+		Estado estado1 = new Estado(null, "Rio Grande Do Sul");
+		Estado estado2 = new Estado(null, "Santa Catarina");
+
+		Cidade cidade1 = new Cidade(null, "Porto Alegre", estado1);
+		Cidade cidade2 = new Cidade(null, "Balneário Camboriú", estado2);
+		Cidade cidade3 = new Cidade(null, "Itajaí", estado2);
+
 		categoria1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		categoria2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(categoria1));
 		p2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
 		p3.getCategorias().addAll(Arrays.asList(categoria1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
-		this.produtoRepository.saveAll(Arrays.asList(p1, p2, p3));		
 
+		estado1.getCidades().addAll(Arrays.asList(cidade1));
+		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
+
+		this.categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		this.produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		this.estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		this.cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
 	}
 
 }
