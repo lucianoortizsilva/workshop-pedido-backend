878006b3ee0e5678ae1c2c4759f2bb738909034d
Luciano Ortiz Silva <lucianoortizsilva@gmail.com>
2019-11-27 14:09:31 -0300

    Deletando cliente e endereco via cascade

--- src/main/java/com/lucianoortizsilva/workshoppedido/service/ClienteService.java
+++ src/main/java/com/lucianoortizsilva/workshoppedido/service/ClienteService.java
@@ -66,7 +66,7 @@ public class ClienteService {
 		try {
 			this.repository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
 	
