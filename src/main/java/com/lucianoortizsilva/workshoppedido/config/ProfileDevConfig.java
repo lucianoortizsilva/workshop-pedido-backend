585bd67a3b30a62b77a33239417b2fd78091ea65
Luciano Ortiz Silva <lucianoortizsilva@gmail.com>
2019-12-02 18:34:56 -0300

    Adicionando Profile-DEV com data base MySQL

---
+++ src/main/java/com/lucianoortizsilva/workshoppedido/config/ProfileDevConfig.java
@@ -0,0 +1,35 @@
+package com.lucianoortizsilva.workshoppedido.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.lucianoortizsilva.workshoppedido.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class ProfileDevConfig {
+
+	@Autowired
+	private DBService service;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+
+		if ("create".equals(this.strategy)) {
+			this.service.instantiateDataBase();
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+}
