129ce81c8790344cf291611f07a7e5eedfab1ed6
Luciano Ortiz Silva <lucianoortizsilva@gmail.com>
2019-12-02 19:51:40 -0300

    Add config. para NÃO recriar banco em producao no HEROKU

--- src/main/java/com/lucianoortizsilva/workshoppedido/config/ProfileProdConfig.java
+++ src/main/java/com/lucianoortizsilva/workshoppedido/config/ProfileProdConfig.java
@@ -3,6 +3,7 @@ package com.lucianoortizsilva.workshoppedido.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,10 +17,19 @@ public class ProfileProdConfig {
 	@Autowired
 	private DBService service;
 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
 	@Bean
 	public boolean instantiateDataBase() throws ParseException {
-		this.service.instantiateDataBase();
-		return true;
+
+		if ("create".equals(this.strategy)) {
+			this.service.instantiateDataBase();
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 
 }
